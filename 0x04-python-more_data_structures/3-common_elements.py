47c535d4b16529f9fabcd0566c4eca45dfe4a999
tafe124 <tafesegechera22@gmail.com>
2022-08-31 06:06:28 -0700

    advanced task

--- 0x04-python-more_data_structures/3-common_elements.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-def common_elements(set_1, set_2):
-    if set_1 and set_2:
-        common = {ele1 for ele1 in set_1 for ele2 in set_2 if ele1 == ele2}
-
-        return common
-    return {} 
