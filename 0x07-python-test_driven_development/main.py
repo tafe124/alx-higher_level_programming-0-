a14aab7ae0c976ee36e4e44178bab59615fbc46e
tafe124 <tafesegechera22@gmail.com>
2022-09-16 06:26:15 -0700

    dot file

--- 0x07-python-test_driven_development/main.py
+++ 0x07-python-test_driven_development/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-max_integer = __import__('6-max_integer').max_integer
+lazy_matrix_mul = __import__('101-lazy_matrix_mul').lazy_matrix_mul
 
-print(max_integer([1, 2, 3, 4]))
-print(max_integer([1, 3, 4, 2]))
+print(lazy_matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
+print(lazy_matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
