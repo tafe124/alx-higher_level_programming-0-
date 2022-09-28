18b429f5465e48ff2e84440c476f8da2aa576921
tafe124 <tafesegechera22@gmail.com>
2022-09-28 05:20:13 -0700

    task is done

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module for Student to Json """
+
+
+class Student:
+    """ Student Doc """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """  retrieves a dictionary representation of a Student instance """
+        if attrs is None:
+            return self.__dict__
+        my_dict = {}
+        for key in self.__dict__.keys():
+            if key in attrs:
+                my_dict[key] = self.__dict__[key]
+        return my_dict
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the Student instance """
+        for attr_name, attr_value in json.items():
+            setattr(self, attr_name, attr_value)
