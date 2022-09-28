5288664142c3346bdbf798580b6bcc8aff1876c3
tafe124 <tafesegechera22@gmail.com>
2022-09-28 05:18:16 -0700

    redone

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-"""Class defination of student"""
+""" Module for Student to Json """
 
 
 class Student:
-    """Defines a student class"""
+    """ Student Doc """
     def __init__(self, first_name, last_name, age):
-        """Initializes instance data"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-            Retrieves the dictionary representation of  student
-        """
-        dictionary = self.__dict__
+        """  retrieves a dictionary representation of a Student instance """
         if attrs is None:
-            return dictionary
-
-        d = {k: v for k, v in dictionary.items() if k in attrs}
-        return d 
+            return self.__dict__
+        my_dict = {}
+        for key in self.__dict__.keys():
+            if key in attrs:
+                my_dict[key] = self.__dict__[key]
+        return my_dict
